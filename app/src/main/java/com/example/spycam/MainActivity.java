601422c1158ed4100b076807c428004d42b29e77
Riya kumari <59931866+Riyakumarint@users.noreply.github.com>
2020-05-16 07:36:15 +0530

    Update MainActivity.java
    some intent input

--- app/src/main/java/com/example/spycam/MainActivity.java
+++ app/src/main/java/com/example/spycam/MainActivity.java
@@ -11,4 +11,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void permanentStore(View view) {
+        Intent intent = new Intent(this, permanentStore.class);
+    }
+    public void temporaryStore(View view) {
+            Intent intent = new Intent (this, TempStore.class);
+    }
+
+    public void start_service(View view) {
+            Intent intent = new Intent (this, ServiceController.class);
+    }
+
+    public void stop_service(View view) {
+            Intent intent = new Intent (this, MainService.class);
+    }
+
+    public void settings(View view) {
+            Intent intent = new Intent (this, settings.class);
+    }
+
 }
